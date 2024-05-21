aff142b29c308be18b3bcac4c666d8a4d313157a
Leonardo Santiago <leonardo@mixrank.com>
2024-05-20 21:36:56 -0300

    update readme

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ pub fn main() -> anyhow::Result<()> {
   let pkgs = eval_from_str("import <nixpkgs>")?
     .call_with(AttrSet::default())?;
   let valid_pkgs = pkgs.items()?
-    .filter_map(|(name, term)| term.ok().map(|t| (name, t))).count();
+    .filter_map(|(name, term)| term.ok().map(|t| (name, t)))
+    .count();
   println!("{valid_pkgs}");
   Ok(())
 }
