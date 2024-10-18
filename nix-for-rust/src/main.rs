5d9b1b0df0f536bb655142d3d62f8acc83c89f45
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2024-10-18 10:39:24 -0300

    remove unnecessary print

--- nix-for-rust/src/main.rs
+++ nix-for-rust/src/main.rs
@@ -4,7 +4,6 @@ pub fn main() -> anyhow::Result<()> {
   let mut state = NixSettings::default()
     .with_setting("extra-experimental-features", "flakes")
     .with_default_store()?;
-  println!("{:?}", state.settings.get_setting("extra-experimental-features"));
   let valid_pkgs = state.eval_string("import <nixpkgs>", std::env::current_dir()?)?
     .call_with(AttrSet::default())?
     .items()?
