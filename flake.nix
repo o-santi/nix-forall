9fb850283f52f13a281ab94da3aed9592ee541c5
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-02-10 10:42:36 -0300

    fix(openssl): update `nixpkgs` and `nix` to use compatible openssl versions

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     nocargo = {
       url = "gitlab:deltaex/nocargo";
